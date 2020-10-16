12b63c6c88d6a9f7e8a006ff8cf0d7d22a961131
yanliu <wyl199825@gmail.com>
2020-10-16 11:37:47 +0800

    test .gitignore file

---
+++ LinkedList/src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * @author yanliu
+ * @create 2020-10-16-11:36
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test .gitignore file");
+    }
+}
