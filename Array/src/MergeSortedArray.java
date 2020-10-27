21337c752a32b35eefe338bf716614379bba6c94
yanliu <wyl199825@gmail.com>
2020-10-27 10:33:41 +0800

    submit the solution of problem-merge two sorted arrays

--- Array/src/MergeSortedArray.java
+++ Array/src/MergeSortedArray.java
@@ -38,28 +38,30 @@
  * @create 2020-10-16-15:31
  */
 public class MergeSortedArray {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
-            return;
-        }
-
-        int i = m-1;
-        int j = n-1;
-        int k = m+n-1;
-
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] < nums2[j]) {
-                nums1[k--] = nums1[j--];
-
-            } else {
-                nums1[k--] = nums2[i--];
-
+    static class Solution {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+                return;
             }
-        }
 
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
-        }
+            int i = m-1;
+            int j = n-1;
+            int k = m+n-1;
 
+            while (i >= 0 && j >= 0) {
+                if (nums1[i] < nums2[j]) {
+                    nums1[k--] = nums1[j--];
+
+                } else {
+                    nums1[k--] = nums2[i--];
+
+                }
+            }
+
+            while (j >= 0) {
+                nums1[k--] = nums2[j--];
+            }
+
+        }
     }
 }
