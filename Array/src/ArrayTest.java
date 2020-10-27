9e86c247803d9ad4b3289ef0c3f5609855485b3f
yanliu <wyl199825@gmail.com>
2020-10-27 10:45:57 +0800

    eliminate Tests

--- Array/src/ArrayTest.java
+++
@@ -1,8 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * @author yanliu
- * @create 2020-10-26-10:57
- */
-public class ArrayTest {
-}
---
+++ LinkedList/src/LinkedListCycle.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author yanliu
+ * @create 2020-10-27-10:40
+ */
+public class LinkedListCycle {
+}
