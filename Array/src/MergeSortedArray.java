b73794ba51a80cf072ef502fd08f84de046ac1a7
yanliu <wyl199825@gmail.com>
2020-10-29 17:34:48 +0800

    fix bugs

--- Array/src/MergeSortedArray.java
+++ Array/src/MergeSortedArray.java
@@ -40,28 +40,27 @@
 public class MergeSortedArray {
     static class Solution {
         public void merge(int[] nums1, int m, int[] nums2, int n) {
-            if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            if (nums1 == null || nums1.length == 0 ||
+                    nums2 == null || nums2.length == 0) {
                 return;
             }
 
-            int i = m-1;
-            int j = n-1;
-            int k = m+n-1;
+            int i = m-1, j = n-1, k = m+n-1;
 
             while (i >= 0 && j >= 0) {
-                if (nums1[i] < nums2[j]) {
-                    nums1[k--] = nums1[j--];
+                if (nums1[i] > nums2[j]) {
+                    nums1[k--] = nums1[i--];
 
                 } else {
-                    nums1[k--] = nums2[i--];
+                    nums1[k--] = nums2[j--];
 
                 }
+
             }
 
             while (j >= 0) {
                 nums1[k--] = nums2[j--];
             }
-
         }
     }
 }
