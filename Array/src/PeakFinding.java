89887b0d3c3bc0872e0811601871b25a2b2c0d03
yanliu <wyl199825@gmail.com>
2020-10-30 15:54:29 +0800

    fix bugs

---
+++ Array/src/PeakFinding.java
@@ -0,0 +1,24 @@
+/**
+ * @author yanliu
+ * @create 2020-10-30-9:09
+ */
+public class PeakFinding {
+    static class Solution {
+        public int peakFinding(int[] nums) {
+            int l = 0, r = nums.length-1;
+
+            while (l < r) {
+                int mid = l + (r-l) / 2;
+
+                if (nums[mid] > nums[mid+1]) {
+                    r = mid;
+
+                } else {
+                    l = mid + 1;
+                }
+            }
+
+            return l;
+        }
+    }
+}
