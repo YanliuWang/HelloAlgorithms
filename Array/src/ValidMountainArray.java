07f825c99e74289259c998442f72a593dc3dbfd7
yanliu <wyl199825@gmail.com>
2020-10-30 16:35:22 +0800

    submit LC941 solution

--- Array/src/ValidMountainArray.java
+++ Array/src/ValidMountainArray.java
@@ -1,8 +1,25 @@
-package PACKAGE_NAME;
-
 /**
  * @author yanliu
  * @create 2020-10-30-16:24
  */
 public class ValidMountainArray {
+    static class Solution {
+        public boolean validMountainArray(int[] A) {
+            int i = 1;
+
+            while (i < A.length && A[i-1] < A[i]) {
+                i++;
+            }
+
+            if (i == 1 || i == A.length) {
+                return false;
+            }
+
+            while (i < A.length && A[i-1] > A[i]) {
+                i++;
+            }
+
+            return i == A.length;
+        }
+    }
 }
