eb60d41e5bf7fb612384314281251fb7aeea47fd
yanliu <wyl199825@gmail.com>
2020-10-30 16:34:33 +0800

    submit LC1346 solution

--- Array/src/CheckIfExist.java
+++ Array/src/CheckIfExist.java
@@ -1,8 +1,25 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author yanliu
  * @create 2020-10-30-16:00
  */
 public class CheckIfExist {
+    static class Solution {
+        public boolean checkIfExist(int[] arr) {
+            Set<Integer> seen = new HashSet<>();
+
+            for (int num : arr) {
+                // set contains 2 * num or half of num
+                if (seen.contains(num * 2) || (num % 2 == 0 && seen.contains(num / 2))) {
+                    return true;
+                }
+
+                seen.add(num);
+            }
+
+            return false;
+        }
+    }
 }
---
+++ Array/src/ValidMountainArray.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author yanliu
+ * @create 2020-10-30-16:24
+ */
+public class ValidMountainArray {
+}
