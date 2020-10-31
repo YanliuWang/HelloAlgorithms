638177326c8766920b836e4657f36b88ac72a7ca
yanliu <wyl199825@gmail.com>
2020-10-31 22:41:38 +0800

    submit LC977 solution

--- Array/src/SortedSquares.java
+++ Array/src/SortedSquares.java
@@ -1,8 +1,30 @@
-package PACKAGE_NAME;
-
 /**
  * @author yanliu
  * @create 2020-10-31-22:03
  */
 public class SortedSquares {
+    static class Solution {
+        public int[] sortedSquares(int[] A) {
+            int n = A.length;
+
+            int[] res = new int[n];
+
+            int left = 0, right = n-1;
+
+            for (int i = n-1; i >= 0; i--) {
+                if (Math.abs(A[left]) > Math.abs(A[right])) {
+                    res[i] = A[left] * A[left];
+                    left++;
+
+                } else {
+                    res[i] = A[right] * A[right];
+                    right--;
+
+                }
+
+            }
+
+            return res;
+        }
+    }
 }
