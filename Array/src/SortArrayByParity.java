33a2fc2d195ed9b300e91b7f5e14a0ee964c37bc
yanliu <wyl199825@gmail.com>
2020-10-31 20:47:43 +0800

    submit LC905 solution

---
+++ Array/src/SortArrayByParity.java
@@ -0,0 +1,43 @@
+/**
+ * @author yanliu
+ * @create 2020-10-31-20:37
+ */
+public class SortArrayByParity {
+    static class Solution {
+        public int[] sortArrayByParity(int[] A) {
+            if (A == null || A.length == 0) {
+                return A;
+            }
+
+            int start = 0, end = A.length-1;
+
+            while (start < end) {
+                if (A[start] % 2 == 0) {
+                    start++;
+
+                } else {
+                    if (A[end] % 2 != 0) {
+                        end--;
+
+                    }
+
+                    if (A[end] % 2 == 0) {
+                        swap(A, start, end);
+                        start++;
+                        end--;
+                    }
+                }
+            }
+
+            return A;
+        }
+
+        private void swap(int[] A, int i, int j) {
+            int tmp = A[i];
+            A[i] = A[j];
+            A[j] = tmp;
+
+        }
+
+    }
+}
