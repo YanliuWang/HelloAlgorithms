7e01d5689ee03f1a2c06097fbc5f316e3cac7cdd
yanliu <wyl199825@gmail.com>
2020-11-01 10:58:06 +0800

    update gitignore

---
+++ Tree/src/InOrderTraversal.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author yanliu
+ * @create 2020-11-01-10:27
+ */
+public class InOrderTraversal {
+}
---
+++ Tree/src/PostOrderTraversal.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author yanliu
+ * @create 2020-11-01-10:26
+ */
+public class PostOrderTraversal {
+}
