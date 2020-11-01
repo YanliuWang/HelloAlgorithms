9beb5c4ba1a5acee026f149c39cd68c96a34b2c3
yanliu <wyl199825@gmail.com>
2020-11-01 11:03:30 +0800

    submit LC144 solution

--- Tree/src/PreOrderTraversal.java
+++ Tree/src/PreOrderTraversal.java
@@ -1,8 +1,62 @@
-package PACKAGE_NAME;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
 /**
  * @author yanliu
  * @create 2020-11-01-10:26
  */
 public class PreOrderTraversal {
+    static class Solution {
+
+        public List<Integer> preOrderTraversal(TreeNode root) {
+            // iteration
+//            List<Integer> res = new ArrayList<>();
+//            Deque<TreeNode> stack = new ArrayDeque<>();
+//            TreeNode curr = root;
+//
+//            while (curr != null || stack.size() != 0) {
+//                if (curr != null) {
+//                    stack.addFirst(curr);
+//                    res.add(curr.val);
+//                    curr = curr.left;
+//
+//                } else {
+//                    curr = stack.removeFirst();
+//                    curr = curr.right;
+//                }
+//            }
+//
+//            return res;
+//
+//        }
+
+            // recursion
+            List<Integer> res = new ArrayList<>();
+
+            _helper(root, res);
+
+            return res;
+
+        }
+
+        private void _helper(TreeNode root, List<Integer> res) {
+            if (root != null) {
+                res.add(root.val);
+
+                if (root.left != null) {
+                    _helper(root.left, res);
+
+                }
+
+                if (root.right != null) {
+                    _helper(root.right, res);
+
+                }
+            }
+        }
+    }
+
+
 }
