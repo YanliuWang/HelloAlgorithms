34cc2f7190cb7a92ff364dfff4cfb288d006dcfe
yanliu <wyl199825@gmail.com>
2020-11-11 13:55:32 +0800

    submit LC116 solution

--- Tree/src/Node.java
+++ Tree/src/Node.java
@@ -1,8 +1,23 @@
-package PACKAGE_NAME;
-
 /**
  * @author yanliu
  * @create 2020-11-11-13:40
  */
-public class Node {
+class Node {
+    int val;
+    Node left;
+    Node right;
+    Node next;
+
+    public Node() {}
+
+    public Node(int val) {
+        this.val = val;
+    }
+
+    public Node(int val, Node left, Node right, Node next) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+        this.next = next;
+    }
 }
