391c8dc7eb18eda8fc76664be040dbe5ed62061c
yanliu <wyl199825@gmail.com>
2020-11-24 09:38:05 +0800

    submit LC155 solution

--- Stack/src/MinStack.java
+++ Stack/src/MinStack.java
@@ -1,8 +1,50 @@
-package PACKAGE_NAME;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * @author yanliu
  * @create 2020-11-24-9:28
  */
 public class MinStack {
+    /**
+     * store all values
+     */
+    Deque<Integer> stack1;
+
+    /**
+    * store the min value
+     */
+    Deque<Integer> stack2;
+
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack() {
+        stack1 = new ArrayDeque<>();
+        stack2 = new ArrayDeque<>();
+    }
+
+    public void push(int x) {
+        stack1.push(x);
+
+        if (stack2.isEmpty() || x <= getMin()) {
+            stack2.push(x);
+        }
+    }
+
+    public void pop() {
+        if (stack1.peek() == getMin()) {
+            stack2.pop();
+        }
+
+        stack1.pop();
+    }
+
+    public int top() {
+        return stack1.peek();
+    }
+
+    public int getMin() {
+        return stack2.peek();
+    }
 }
