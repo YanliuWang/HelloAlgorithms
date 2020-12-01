449e1956547dd1d76a573f70b5bf7d9320d39afd
yanliu <wyl199825@gmail.com>
2020-12-01 21:33:31 +0800

    submit selection sort

--- Sort/src/selectionsort/SelectionSort.java
+++ Sort/src/selectionsort/SelectionSort.java
@@ -1,5 +1,48 @@
-package selectionsort;/**
+package selectionsort;
+
+import edu.princeton.cs.algs4.StdRandom;
+import utils.SortUtils;
+
+/**
  * @author yanliu
  * @create 2020-12-01-18:42
- */public class SelectionSort {
+ */
+public class SelectionSort {
+    public static void sort(Comparable[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (SortUtils.less(arr[j], arr[minIdx])) {
+                    minIdx = j;
+                }
+            }
+
+            if (minIdx != i) {
+                SortUtils.swap(arr, minIdx, i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Double a[] = new Double[10];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = StdRandom.uniform();
+        }
+
+        sort(a);
+
+        if (SortUtils.isSorted(a)) {
+            System.out.println("sort succeeded");
+
+        } else {
+            System.out.println("sort failed");
+
+        }
+    }
 }
---
+++ Sort/src/shellsort/ShellSort.java
@@ -0,0 +1,5 @@
+package shellsort;/**
+ * @author yanliu
+ * @create 2020-12-01-21:33
+ */public class ShellSort {
+}
