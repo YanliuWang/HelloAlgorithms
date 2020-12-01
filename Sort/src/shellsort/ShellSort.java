d202216559f7f48a52856a770d9101277cea6504
yanliu <wyl199825@gmail.com>
2020-12-01 22:07:21 +0800

    submit shell sort

--- Sort/src/shellsort/ShellSort.java
+++ Sort/src/shellsort/ShellSort.java
@@ -1,5 +1,57 @@
-package shellsort;/**
+package shellsort;
+
+import edu.princeton.cs.algs4.StdRandom;
+import utils.SortUtils;
+
+/**
  * @author yanliu
  * @create 2020-12-01-21:33
- */public class ShellSort {
+ */
+public class ShellSort {
+    public static void sort(Comparable[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        int h = 1;
+        int N = arr.length;
+
+        while (h < 3 / N) {
+            h = 3 * h + 1;
+        }
+
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                Comparable key = arr[i];
+
+                int j;
+
+                for (j = i - h; j >= 0 && SortUtils.less(key, arr[j]); j -= h) {
+                    arr[j + h] = arr[j];
+                }
+
+                arr[j + h] = key;
+            }
+
+            h = h / 3;
+        }
+    }
+
+    public static void main(String[] args) {
+        Double a[] = new Double[10];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = StdRandom.uniform();
+        }
+
+        sort(a);
+
+        if (SortUtils.isSorted(a)) {
+            System.out.println("sort succeeded");
+
+        } else {
+            System.out.println("sort failed");
+
+        }
+    }
 }
