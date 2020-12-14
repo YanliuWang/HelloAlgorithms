bdf2185e355d534e0e7bef00d1929cdc6b57c201
yanliu <wyl199825@gmail.com>
2020-12-14 14:14:26 +0800

    submit LC236 solution

---
+++ Tree/src/LowestCommonAncestor.java
@@ -0,0 +1,26 @@
+/**
+ * @author yanliu
+ * @create 2020-12-14-14:10
+ */
+public class LowestCommonAncestor {
+    static class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null || root == p || root == q) {
+                return root;
+            }
+
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+            if (left == null && right == null) {
+                return null;
+            }
+
+            if (left != null && right != null) {
+                return root;
+            }
+
+            return left == null ? right : left;
+        }
+    }
+}
