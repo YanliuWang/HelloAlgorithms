cae1a06fd44ea1f46572fb2ab4f2e3d31cd08192
yanliu <wyl199825@gmail.com>
2020-12-14 14:34:53 +0800

    add anotation

--- Tree/src/LowestCommonAncestor.java
+++ Tree/src/LowestCommonAncestor.java
@@ -1,7 +1,10 @@
 /**
  * @author yanliu
- * @create 2020-12-14-14:10
+ * @create 2020-12-14-14:1
+ * using postOrder traverse the binary tree
+ * from bottom to top
  */
+
 public class LowestCommonAncestor {
     static class Solution {
         public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
@@ -12,14 +15,19 @@ public class LowestCommonAncestor {
             TreeNode left = lowestCommonAncestor(root.left, p, q);
             TreeNode right = lowestCommonAncestor(root.right, p, q);
 
+            // p and q are both not in root tree
+            // return null
             if (left == null && right == null) {
                 return null;
             }
 
+            // p and q are both in root tree
+            // return the root
             if (left != null && right != null) {
                 return root;
             }
 
+            // return the non-null node
             return left == null ? right : left;
         }
     }
