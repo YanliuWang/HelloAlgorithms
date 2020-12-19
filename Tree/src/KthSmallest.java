f81f1b16ba54e87f287f54bf821284f95ee53bf4
yanliu <wyl199825@gmail.com>
2020-12-19 23:02:26 +0800

    submit LC230 solution

--- Tree/src/KthSmallest.java
+++ Tree/src/KthSmallest.java
@@ -1,5 +1,32 @@
-package PACKAGE_NAME;/**
+/**
  * @author yanliu
  * @create 2020-12-16-17:06
- */public class KthSmallest {
+ */
+public class KthSmallest {
+    static class Solution {
+        private int res = 0;
+        private int count = 0;
+
+        public int kthSmallest(TreeNode root, int k) {
+            count = k;
+
+            helper(root);
+
+            return res;
+        }
+
+        private void helper(TreeNode root) {
+            if (root.left != null) {
+                helper(root.left);
+            }
+
+            if (--count == 0) {
+                res = root.val;
+            }
+
+            if (root.right != null) {
+                helper(root.right);
+            }
+        }
+    }
 }
