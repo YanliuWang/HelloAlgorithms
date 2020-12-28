30da0732977125955facb7787b1c6a61a625b87a
yanliu <wyl199825@gmail.com>
2020-12-28 12:22:04 +0800

    format LC24 solution

--- LinkedList/src/SwapPairs.java
+++ LinkedList/src/SwapPairs.java
@@ -1,5 +1,50 @@
-package PACKAGE_NAME;/**
+/**
  * @author yanliu
  * @create 2020-12-27-15:28
- */public class SwapPairs {
+ */
+public class SwapPairs {
+    static class Solution {
+        public ListNode swapPairs(ListNode head) {
+            if (head == null) {
+                return null;
+            }
+
+            return reverseTwoGroup(head, 2);
+        }
+
+        private ListNode reverseTwoGroup(ListNode head, int k) {
+            if (head == null) {
+                return null;
+            }
+
+            ListNode a = head, b = head;
+
+            for (int i = 0; i < k; i++) {
+                if (b == null) {
+                    return head;
+                }
+
+                b = b.next;
+            }
+
+            ListNode newHead = reverseBetween(a, b);
+            a.next = reverseTwoGroup(b, k);
+
+            return newHead;
+        }
+
+        private ListNode reverseBetween(ListNode a, ListNode b) {
+            ListNode curr = a, pre = null, next = null;
+
+            while (curr != b) {
+                next = curr.next;
+                curr.next = pre;
+                pre = curr;
+                curr = next;
+            }
+
+            return pre;
+        }
+    }
+
 }
