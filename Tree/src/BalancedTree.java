7c2477f45e9822fd4d1a49af56905f10ca64ba46
yanliu <wyl199825@gmail.com>
2021-01-01 23:18:05 +0800

    submit LC110 solution

--- Tree/src/BalancedTree.java
+++ Tree/src/BalancedTree.java
@@ -1,5 +1,23 @@
-package PACKAGE_NAME;/**
+/**
  * @author yanliu
  * @create 2021-01-01-12:13
- */public class BalancedTree {
+ */
+public class BalancedTree {
+    static class Solution {
+        public boolean isBalanced(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+
+            return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+
+        private int height(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            return Math.max(height(root.left), height(root.right)) + 1;
+        }
+    }
 }
