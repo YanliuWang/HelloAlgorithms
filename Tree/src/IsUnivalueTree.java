e101ccf2fd708a4f85e0cf4866f3e49c7aa58cb5
yanliu <wyl199825@gmail.com>
2021-01-04 11:37:22 +0800

    submit LC572 solution

--- Tree/src/IsUnivalueTree.java
+++ Tree/src/IsUnivalueTree.java
@@ -1,5 +1,28 @@
-package PACKAGE_NAME;/**
+/**
+ * LC965
  * @author yanliu
  * @create 2021-01-04-10:29
- */public class IsUnivalueTree {
+ */
+public class IsUnivalueTree {
+    static class Solution {
+        private int val;
+
+        public boolean isUnivalueTree(TreeNode root) {
+            val = root.val;
+
+            return dfs(root);
+        }
+
+        private boolean dfs(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+
+            if (root.val != val) {
+                return false;
+            }
+
+            return dfs(root.left) && dfs(root.right);
+        }
+    }
 }
