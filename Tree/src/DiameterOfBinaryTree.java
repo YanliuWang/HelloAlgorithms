7cc1fbaf3d2ffaf1a56c2e62505af67fb602896e
yanliu <wyl199825@gmail.com>
2021-01-25 20:58:33 +0800

    submit LC543 solution

--- Tree/src/DiameterOfBinaryTree.java
+++ Tree/src/DiameterOfBinaryTree.java
@@ -1,5 +1,29 @@
-package PACKAGE_NAME;/**
+/**
  * @author yanliu
  * @create 2021-01-25-20:52
- */public class DiameterOfBinaryTree {
+ */
+public class DiameterOfBinaryTree {
+    static class Solution {
+        private int res = 0;
+
+        public int diameterOfBinaryTree(TreeNode root) {
+            getMaxDiameter(root);
+
+            return res;
+        }
+
+        // the max diameter of left and right subtree rooted in node
+        private int getMaxDiameter(TreeNode node) {
+            if (node == null) {
+                return 0;
+            }
+
+            int left = getMaxDiameter(node.left);
+            int right = getMaxDiameter(node.right);
+
+            res = Math.max(res, left + right);
+
+            return Math.max(left, right) + 1;
+        }
+    }
 }
