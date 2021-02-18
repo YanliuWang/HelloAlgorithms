aa94d6c3156b43ffaa46a8e25c4f089d536bc770
yanliu <wyl199825@gmail.com>
2021-02-18 10:26:59 +0800

    commit merge sort

--- Sort/src/mergesort/MergeSort.java
+++ Sort/src/mergesort/MergeSort.java
@@ -8,49 +8,70 @@ import utils.SortUtils;
  * @create 2020-11-28-10:20
  */
 public class MergeSort {
+    /**
+     * recursion merge sort
+     * top down
+     * @param arr
+     */
+    private static Comparable[] helper;
+
     public static void sortRecursion(Comparable[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
 
-        sort(arr, 0, arr.length - 1);
+        helper = new Comparable[arr.length];
 
+        sort(arr, 0, arr.length - 1);
     }
 
-    private static void sort(Comparable[] arr, int left, int right) {
-        if (left == right) {
+    private static void sort(Comparable[] arr, int lo, int hi) {
+        if (lo == hi) {
             return;
         }
 
-        int mid = left + (right - left) / 2;
-        sort(arr, left, mid);
-        sort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
+        int mid = lo + (hi - lo) / 2;
+        sort(arr, lo, mid);
+        sort(arr, mid + 1, hi);
+        merge(arr, lo, mid, hi);
     }
 
-    private static void merge(Comparable[] arr, int left, int mid, int right) {
-        Comparable[] helper = new Comparable[right - left + 1];
+    private static void merge(Comparable[] arr, int lo, int mid, int hi) {
+        int i = lo, j = mid + 1;
 
-        int i = 0, j = left, k = mid + 1;
-
-        while (j <= mid && k <= right) {
-            helper[i++] = SortUtils.less(arr[k], arr[j]) ? arr[k++] : arr[j++];
+        // move all elements in arr to helper
+        for (int k = lo; k <= hi; k++) {
+            helper[k] = arr[k];
         }
 
-        while (j <= mid) {
-            helper[i++] = arr[j++];
-        }
+        // merge
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                // the left side elements are all used
+                // put the right side elements to arr
+                arr[k] = helper[j++];
 
-        while (k <= right) {
-            helper[i++] = arr[k++];
-        }
+            } else if (j > hi) {
+                // the right side elements are all used
+                // put the left side elements to arr
+                arr[k] = helper[i++];
 
-        for (i = 0; i < helper.length; i++) {
-            arr[i + left] = helper[i];
+            } else if (SortUtils.less(helper[j], helper[i])) {
+                arr[k] = helper[j++];
+
+            } else {
+                arr[k] = helper[i++];
+            }
+
         }
 
     }
 
+    /**
+     * iterative merge sort
+     * from bottom up
+     * @param arr
+     */
     public static void sortIteration(Comparable[] arr) {
         if (arr == null || arr.length < 2) {
             return;
@@ -58,7 +79,11 @@ public class MergeSort {
 
         int N = arr.length;
 
+        helper = new Comparable[N];
+
+        // from bottom up to increase the sort sequence size
         for (int currSize = 1; currSize < N; currSize *= 2) {
+            // lo < N-currSize makes sure mid can be cover the last element
             for (int lo = 0; lo < N - currSize; lo += currSize * 2) {
                 merge(arr, lo, lo + currSize - 1, Math.min(lo + currSize + currSize - 1, N - 1));
             }
@@ -66,13 +91,13 @@ public class MergeSort {
     }
 
     public static void main(String[] args) {
-        Double a[] = new Double[10];
+        Double[] a = new Double[10];
 
         for (int i = 0; i < a.length; i++) {
             a[i] = StdRandom.uniform();
         }
 
-        sortIteration(a);
+        sortRecursion(a);
 
         if (SortUtils.isSorted(a)) {
             System.out.println("sort succeeded");
@@ -80,6 +105,10 @@ public class MergeSort {
         } else {
             System.out.println("sort failed");
 
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
         }
     }
 }
---
+++ Sort/src/quicksort/QuickSort3Way.java
@@ -0,0 +1,5 @@
+package quicksort;/**
+ * @author yanliu
+ * @create 2021-02-18-9:48
+ */public class QuickSort3Way {
+}
