a50e27d3813ccf5af7925d5145d914daf6833034
yanliu <wyl199825@gmail.com>
2021-04-07 10:36:43 +0800

    submit LC106 solution

--- Tree/src/BuildTreeFromInorderAndPostorder.java
+++ Tree/src/BuildTreeFromInorderAndPostorder.java
@@ -1,43 +1,41 @@
 /**
+ * LC106
  * @author yanliu
  * @create 2020-11-06-10:57
  */
 public class BuildTreeFromInorderAndPostorder {
     static class Solution {
-        public TreeNode buildTree(int[] inorder, int[] postOrder) {
-            if (inorder == null || postOrder == null || inorder.length != postOrder.length) {
+        public TreeNode buildTree(int[] inorder, int[] postorder) {
+            if (inorder == null || postorder == null || inorder.length != postorder.length) {
                 return null;
             }
 
-            return _helper(inorder, postOrder, 0, 0, inorder.length);
+            int inStart = 0, inEnd = inorder.length - 1;
+            int postStart = 0, postEnd = postorder.length - 1;
+
+            return buildTree(inorder, inStart, inEnd, postorder, postStart, postEnd);
         }
 
-        private TreeNode _helper(int[] inorder, int[] postOrder, int inorderStart, int postOrderStart, int size) {
-            if (size == 0) {
+        private TreeNode buildTree(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
+            if (inStart > inEnd || postStart > postEnd) {
                 return null;
             }
 
-            TreeNode root = new TreeNode(postOrder[postOrderStart + size - 1]);
+            int rootVal = postorder[postEnd];
+            TreeNode root = new TreeNode(rootVal);
 
-            int inOrderRootIdx = inorderStart;
+            int inOrderRootIdx = 0;
 
-            while (inorder[inOrderRootIdx] != postOrder[postOrderStart + size - 1]) {
-                inOrderRootIdx++;
+            for (int i = inStart; i <= inEnd; i++) {
+                if (inorder[i] == rootVal) {
+                    inOrderRootIdx = i;
+                }
             }
 
-            int leftSize = inOrderRootIdx - inorderStart;
-            int rightSize = size - leftSize -1;
-
-            TreeNode leftNode = _helper(inorder, postOrder, inorderStart, postOrderStart, leftSize);
-            TreeNode rightNode = _helper(inorder, postOrder, inOrderRootIdx+1,
-                    postOrderStart+leftSize, rightSize);
-
-            root.left = leftNode;
-            root.right = rightNode;
+            root.left = buildTree(inorder, inStart, inOrderRootIdx - 1, postorder, postStart, postStart + inOrderRootIdx - (inStart + 1));
+            root.right = buildTree(inorder, inOrderRootIdx + 1, inEnd, postorder, postStart + inOrderRootIdx - inStart, postEnd - 1);
 
             return root;
-
-
         }
     }
 }
