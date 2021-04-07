40ec0874c63226d2a19b54db1f3446672fe60014
yanliu <wyl199825@gmail.com>
2021-04-07 16:47:39 +0800

    submit LC105 solution

--- Tree/src/BuildTreeFromPreorderAndInorder.java
+++ Tree/src/BuildTreeFromPreorderAndInorder.java
@@ -5,39 +5,39 @@
  */
 public class BuildTreeFromPreorderAndInorder {
     static class Solution {
-            public TreeNode buildTree(int[] preorder, int[] inorder) {
-                if (preorder == null || inorder == null || preorder.length != inorder.length) {
-                    return null;
-                }
-
-                int preStart = 0, preEnd = preorder.length - 1;
-                int inStart = 0, inEnd = inorder.length - 1;
-
-                return buildTree(preorder, preStart, preEnd, inorder, inStart, inEnd);
+        public TreeNode buildTree(int[] preorder, int[] inorder) {
+            if (preorder == null || inorder == null || preorder.length != inorder.length) {
+                return null;
             }
 
-            private TreeNode buildTree(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
-                if (preStart > preEnd || inStart > inEnd) {
-                    return null;
-                }
+            int preStart = 0, preEnd = preorder.length - 1;
+            int inStart = 0, inEnd = inorder.length - 1;
 
-                int rootVal = preorder[preStart];
-                TreeNode root = new TreeNode(rootVal);
+            return buildTree(preorder, preStart, preEnd, inorder, inStart, inEnd);
+        }
 
-                int inorderRootIdx = 0;
-                for (int i = inStart; i <= inEnd; i++) {
-                    if (inorder[i] == rootVal) {
-                        inorderRootIdx = i;
-                        break;
-                    }
-                }
-
-                root.left = buildTree(preorder, preStart + 1, preStart + inorderRootIdx - inStart, inorder, inStart, inorderRootIdx - 1);
-                root.right = buildTree(preorder, preStart + inorderRootIdx - inStart + 1, preEnd, inorder, inorderRootIdx + 1, inEnd);
-
-                return root;
-
+        private TreeNode buildTree(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
+            if (preStart > preEnd || inStart > inEnd) {
+                return null;
             }
+
+            int rootVal = preorder[preStart];
+            TreeNode root = new TreeNode(rootVal);
+
+            int inorderRootIdx = 0;
+            for (int i = inStart; i <= inEnd; i++) {
+                if (inorder[i] == rootVal) {
+                    inorderRootIdx = i;
+                    break;
+                }
+            }
+
+            root.left = buildTree(preorder, preStart + 1, preStart + inorderRootIdx - inStart, inorder, inStart, inorderRootIdx - 1);
+            root.right = buildTree(preorder, preStart + inorderRootIdx - inStart + 1, preEnd, inorder, inorderRootIdx + 1, inEnd);
+
+            return root;
+
         }
     }
 }
+
