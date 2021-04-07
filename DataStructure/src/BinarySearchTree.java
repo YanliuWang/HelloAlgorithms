0c08f349dd630e5bbb3a59dd9a4893799586ae4e
yanliu <wyl199825@gmail.com>
2021-04-07 16:48:17 +0800

    submit binary search tree operations

--- DataStructure/src/BinarySearchTree.java
+++ DataStructure/src/BinarySearchTree.java
@@ -1,5 +1,145 @@
-package PACKAGE_NAME;/**
+/**
  * @author yanliu
  * @create 2021-04-07-15:09
- */public class BinarySearchTree {
+ */
+public class BinarySearchTree {
+    public static boolean find(TreeNode root, int target) {
+        TreeNode curr = root;
+
+        while (curr != null) {
+            if (target < root.val) {
+                curr = curr.left;
+
+            } else if (target > root.val) {
+                curr = curr.right;
+
+            } else {
+                return true;
+
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean add(TreeNode root, int value) {
+        if (root == null) {
+            root = new TreeNode(value);
+            return true;
+        }
+
+        TreeNode curr = root;
+
+        while (curr != null) {
+            if (value < root.val) {
+                if (curr.left == null) {
+                    curr.left = new TreeNode(value);
+                    return true;
+                }
+
+                curr = curr.left;
+
+            } else if (value > root.val) {
+                if (curr.right == null) {
+                    curr.right = new TreeNode(value);
+                    return true;
+                }
+
+                curr = curr.right;
+
+            } else {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean remove(TreeNode root, int target) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.val == target) {
+            root = removeNode(root);
+            return true;
+        }
+
+        TreeNode curr = root;
+
+        while (curr != null) {
+            if (target < curr.val) {
+                if (curr.left != null && curr.left.val != target) {
+                    curr = curr.left;
+
+                } else if (curr.left == null) {
+                    return false;
+
+                } else {
+                    curr.left = removeNode(curr.left);
+                    return true;
+                }
+
+            } else if (target > curr.val) {
+                if (curr.right != null && curr.right.val != target) {
+                    curr = curr.right;
+
+                } else if (curr.right == null) {
+                    return false;
+
+                } else {
+                    curr.right = removeNode(curr.right);
+                    return true;
+                }
+
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * remove the node and return new root
+     * @param node
+     * @return
+     */
+    private static TreeNode removeNode(TreeNode node) {
+        if (node.left == null && node.right == null) {
+            return null;
+
+        } else if (node.left == null) {
+            return node.right;
+
+        } else if (node.right == null) {
+            return node.left;
+
+        } else {
+            node.val = removeNodeWithTwoChildren(node);
+            return node;
+
+        }
+    }
+
+    private static int removeNodeWithTwoChildren(TreeNode node) {
+        int result = 0;
+
+        if (node.left.right == null) {
+            result = node.left.val;
+            node.left = node.left.left;
+            return result;
+        }
+
+        TreeNode curr = node.left;
+
+        while (curr.right.right != null) {
+            curr = curr.right;
+        }
+
+        result = curr.right.val;
+
+        curr.right = curr.right.left;
+
+        return result;
+
+    }
 }
