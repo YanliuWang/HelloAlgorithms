8e326a815e22a2c4545aa4d9b70f127237a02fa9
yanliu <wyl199825@gmail.com>
2021-04-11 12:33:28 +0800

    submit LintCode 1359 solution

---
+++ Tree/src/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,34 @@
+/**LintCode 1359
+ * @author yanliu
+ * @create 2021-04-11-12:32
+ */
+public class ConvertSortedArrayToBinarySearchTree {
+    static class Solution {
+        /**
+         * @param nums: the sorted array
+         * @return: the root of the tree
+         */
+        public TreeNode convertSortedArraytoBinarySearchTree(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return null;
+            }
+
+            return convertToBST(nums, 0, nums.length - 1);
+        }
+
+        private TreeNode convertToBST(int[] nums, int low, int hi) {
+            if (low > hi) {
+                return null;
+            }
+
+            int mid = low + (hi - low) / 2;
+
+            TreeNode root = new TreeNode(nums[mid]);
+
+            root.left = convertToBST(nums, low, mid - 1);
+            root.right = convertToBST(nums, mid + 1, hi);
+
+            return root;
+        }
+    }
+}
