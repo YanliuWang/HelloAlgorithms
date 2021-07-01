1681fba01f367f767151e5e38466904febf13937
YanliuWang <wyl199825@gmail.com>
2021-07-01 20:25:18 +0800

    submit two sum

--- Array/src/TwoSum.java
+++ Array/src/TwoSum.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author yanliu
@@ -63,7 +61,99 @@ public class TwoSum {
         }
     }
 
-    public static void main(String[] args) {
-        new Solution2().twoSum(new int[]{3, 2, 4}, 6);
+    /**
+     * Lintcode 607: two sum with two operations
+     */
+    class Solution3 {
+        // use list to store the nums
+        private List<Integer> nums = new ArrayList<>();
+
+        /**
+         * the list is increasing order
+         * @param number: An integer
+         * @return: nothing
+         */
+        public void add(int number) {
+            // write your code here
+            // put the number to the list tail
+            nums.add(number);
+
+            // get the number index
+            int index = nums.size() - 1;
+
+            while (index > 0 && nums.get(index) < nums.get(index - 1)) {
+                swap(nums, index);
+                index--;
+            }
+        }
+
+        private void swap(List<Integer> nums, int index) {
+            int temp = nums.get(index);
+            nums.set(index, nums.get(index - 1));
+            nums.set(index - 1, temp);
+
+        }
+
+        /**
+         * @param value: An integer
+         * @return: Find if there exists any pair of numbers which sum is equal to the value.
+         */
+        public boolean find(int value) {
+            // write your code here
+            int left = 0, right = nums.size() - 1;
+
+            while (left < right) {
+                int sum = nums.get(left) + nums.get(right);
+
+                if (sum > value) {
+                    right--;
+
+                } else if (sum < value) {
+                    left++;
+
+                } else {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
+
+    /**
+     * Lintcode 607: two sum with two operations
+     * use hashmap
+     */
+    class Solution4 {
+        private Map<Integer, Integer> numToFreq = new HashMap<>();
+
+        /**
+         * @param number: An integer
+         * @return: nothing
+         */
+        public void add(int number) {
+            // write your code here
+            numToFreq.put(number, numToFreq.getOrDefault(number, 0) + 1);
+        }
+
+        /**
+         * @param value: An integer
+         * @return: Find if there exists any pair of numbers which sum is equal to the value.
+         */
+        public boolean find(int value) {
+            // write your code here
+            for (Integer num1 : numToFreq.keySet()) {
+                int complement = value - num1;
+
+                int complementFreq = complement == num1 ? 2 : 1;
+
+                if (numToFreq.getOrDefault(complement, 0) >= complementFreq) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
 }
