b046893797127716d2ae92fb66fcd01a4c82b36c
YanliuWang <wyl199825@gmail.com>
2021-07-09 16:55:04 +0800

    submit word ladder solution with bfs

--- BFS/src/WordLadder.java
+++ BFS/src/WordLadder.java
@@ -1,44 +1,48 @@
 import java.util.*;
 
 /**
- * LC127
+ * LeetCode127
  * @author yanliu
  * @create 2021-03-31-23:42
  */
 public class WordLadder {
-    static class Solution {
+    class Solution {
         public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-            Set<String> wordSet = new HashSet<>();
-            for (String word : wordList) {
-                wordSet.add(word);
-            }
-
+            // convert list to set
+            Set<String> dict = new HashSet<>(wordList);
             Queue<String> queue = new LinkedList<>();
-            queue.offer(beginWord);
+            // record visited words
+            Set<String> visited = new HashSet<>(wordList.size());
 
-            Set<String> visited = new HashSet<>();
+            queue.offer(beginWord);
             visited.add(beginWord);
 
-            int distance = 1;
+            // length from begin word to end word
+            int length = 1;
 
             while (!queue.isEmpty()) {
+                // to next word length
+                length++;
+
+                // record current level size
                 int size = queue.size();
-                distance++;
 
                 for (int i = 0; i < size; i++) {
-                    String top = queue.poll();
+                    String currWord = queue.poll();
+                    List<String> nextWords = getNextWords(currWord, dict);
 
-                    List<String> wordsWithinDistance = getWordsWithinDistance(top, wordSet);
-
-                    for (String word : wordsWithinDistance) {
-                        if (word.equals(endWord)) {
-                            return distance;
+                    for (String nextWord : nextWords) {
+                        // check duplication
+                        if (visited.contains(nextWord)) {
+                            continue;
                         }
 
-                        if (!visited.contains(word)) {
-                            queue.offer(word);
-                            visited.add(word);
+                        if (endWord.equals(nextWord)) {
+                            return length;
                         }
+
+                        queue.offer(nextWord);
+                        visited.add(nextWord);
                     }
                 }
             }
@@ -46,31 +50,31 @@ public class WordLadder {
             return 0;
         }
 
-        private List<String> getWordsWithinDistance(String word, Set<String> wordSet) {
-            char[] charArray = word.toCharArray();
-            List<String> res = new ArrayList<>();
+        private List<String> getNextWords(String currWord, Set<String> wordList) {
+            List<String> nextWords = new LinkedList<>();
 
-            for (int i = 0; i < charArray.length; i++) {
-                char original = charArray[i];
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                for (int i = 0; i < currWord.length(); i++) {
+                    if (currWord.charAt(i) != ch) {
+                        // replace i-th char in currWord to ch
+                        String word = replace(currWord, i, ch);
 
-                for (char c = 'a'; c <= 'z'; c++) {
-                    if (c == original) {
-                        continue;
-                    }
-
-                    charArray[i] = c;
-
-                    String transformedWord = new String(charArray);
-
-                    if (wordSet.contains(transformedWord)) {
-                        res.add(transformedWord);
+                        if (wordList.contains(word)) {
+                            nextWords.add(word);
+                        }
                     }
                 }
-
-                charArray[i] = original;
             }
 
-            return res;
+            return nextWords;
+        }
+
+        // replace char at position index in word to ch
+        private String replace(String word, int index, char ch) {
+            char[] charArray = word.toCharArray();
+            charArray[index] = ch;
+
+            return new String(charArray);
         }
     }
 }
