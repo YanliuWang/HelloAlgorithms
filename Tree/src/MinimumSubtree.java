ba5bf5204466380de81982fb2ad0bce16fa7545b
YanliuWang <wyl199825@gmail.com>
2021-07-15 10:22:51 +0800

    submit solution2

--- Tree/src/MinimumSubtree.java
+++ Tree/src/MinimumSubtree.java
@@ -4,7 +4,7 @@
  * @create 2021-07-15-9:24
  */
 public class MinimumSubtree {
-    static class Solution {
+    static class Solution1 {
         // record root node with minimum sum
         private TreeNode minRoot;
 
@@ -41,6 +41,58 @@ public class MinimumSubtree {
             }
 
             return minSum;
+        }
+    }
+
+    static class Solution2 {
+        class ResultType {
+            // 最小子树根
+            TreeNode minSubtree;
+
+            int minSum, rootSum;
+
+            public ResultType(TreeNode minSubtree, int minSum, int rootSum) {
+                this.minSubtree = minSubtree;
+                this.minSum = minSum;
+                this.rootSum = rootSum;
+            }
+        }
+
+        /**
+         * @param root: the root of binary tree
+         * @return: the root of the minimum subtree
+         */
+        public TreeNode findSubtree(TreeNode root) {
+            // write your code here
+            ResultType resultType = getResult(root);
+
+            return resultType.minSubtree;
+        }
+
+        private ResultType getResult(TreeNode root) {
+            if (root == null) {
+                return new ResultType(null, Integer.MAX_VALUE, 0);
+            }
+
+            ResultType left = getResult(root.left);
+            ResultType right = getResult(root.right);
+
+            TreeNode minNode = root;
+            int minSum = left.rootSum + right.rootSum + root.val;
+            int currSum = minSum;
+
+            if (left.minSum < minSum) {
+                minSum = left.minSum;
+                minNode = left.minSubtree;
+            }
+
+            if (right.minSum < minSum) {
+                minSum = right.minSum;
+                minNode = right.minSubtree;
+            }
+
+            return new ResultType(minNode, minSum, currSum);
+
         }
     }
 }
