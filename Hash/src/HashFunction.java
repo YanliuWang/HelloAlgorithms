83ea28b89f22a227a618a53558ee9f2af28f8ec9
YanliuWang <wyl199825@gmail.com>
2021-07-29 22:12:13 +0800

    submit LintCode128 solution

--- Hash/src/HashFunction.java
+++ Hash/src/HashFunction.java
@@ -1,5 +1,26 @@
-package PACKAGE_NAME;/**
+/**
+ * LintCode 128
  * @author yanliu
-   @create 2021-07-29-21:45
- */public class HashFunction {
+ * @create 2021-07-29-21:45
+ */
+public class HashFunction {
+    static class Solution {
+        /**
+         * @param key: A string you should hash
+         * @param HASH_SIZE: An integer
+         * @return: An integer
+         */
+        public int hashCode(char[] key, int HASH_SIZE) {
+            // write your code here
+            // 固定且无规律的，同一个 key 对应同一个 hashCode
+            // magic number 是经验值，找一个冲突小的数
+            long ans = 0;
+
+            for (int i = 0; i < key.length; i++) {
+                ans = (ans * 33 + (int) key[i]) % HASH_SIZE;
+            }
+
+            return (int) ans;
+        }
+    }
 }
